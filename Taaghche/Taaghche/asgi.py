7e692e735ef595302afe33c95fbdb52925740a08
raminhrd <raminhr.work@gmail.com>
2025-09-20 00:07:00 +0330

    add_book_app_and_restframework

---
+++ Taaghche/Taaghche/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Taaghche project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taaghche.settings')
+
+application = get_asgi_application()
